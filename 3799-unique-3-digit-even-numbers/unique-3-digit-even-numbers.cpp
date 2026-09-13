b6c2e1847e10e92ffe56747038b5223aac2179c5
HemantGoyal-alt <hemantgoyalbkn@gmail.com>
2026-09-13 19:47:34 +0530

    Time: 77 ms (8.79%) | Memory: 61.8 MB (5.28%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        
-        int freq[10] = {0};
 
-        // Count how many times each digit appears
+        // Step 1: Count the frequency of each digit
+        vector<int> freq(10, 0);
+
         for (int digit : digits) {
             freq[digit]++;
         }
 
         int count = 0;
-
-        // Choose the hundreds digit
-        for (int first = 1; first <= 9; first++) {
-
-            if (freq[first] == 0)
+        for (int num = 100;num<=999;num++){
+            if (num%2!=0){
                 continue;
-
-            freq[first]--;
-
-            // Choose the tens digit
-            for (int second = 0; second <= 9; second++) {
-
-                if (freq[second] == 0)
-                    continue;
-
-                freq[second]--;
-
-                // Choose the units digit
-                for (int third = 0; third <= 8; third += 2) {
-
-                    if (freq[third] > 0) {
-                        count++;
-                    }
-                }
-
-                // Put the tens digit back
-                freq[second]++;
             }
-
-            // Put the hundreds digit back
-            freq[first]++;
+            int n = num;
+            int a = n%10;
+            n /=10;
+            int b = n%10;
+            n /=10;
+            int c= n%10;
+            vector<int>need(10,0);
+            need[a]++;
+            need[b]++;
+            need[c]++;
+            bool posible = true;
+            for (int d=0;d<=9;d++){
+                if(need[d]>freq[d]){
+                    posible=false;
+                    break;
+                }
+            }
+            if (posible){
+                count++;
+            }
         }
-
-        return count;
+        return count ;
     }
 };
